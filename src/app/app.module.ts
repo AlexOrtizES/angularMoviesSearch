322bd5de8e7bc60a500206f0a2fbd5b7d9d1e6b3
aortiz <aortiz@casenetllc.com>
2020-02-25 08:35:26 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import {MaterialModule} from './material-module';
+import { AddMovieFormComponent } from './components/movie/add-movie-form/add-movie-form.component';
+import { ViewMovieComponent } from './components/movie/view-movie/view-movie.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TableComponent } from './components/table/table.component';
+import { MovieFormComponent } from './components/movie-form/movie-form.component';
+import { ListViewComponent } from './components/list-view/list-view.component';
+import { MovieBadgeComponent } from './components/movie-badge/movie-badge.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddMovieFormComponent,
+    ViewMovieComponent,
+    TableComponent,
+    MovieFormComponent,
+    ListViewComponent,
+    MovieBadgeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
